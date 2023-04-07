88d65cfba34da6f2d666e3ca4fd868836bc7da37
Bykit <731240932@qq.com>
2023-04-07 15:17:02 +0800

    添加语雀笔记

--- script.js
+++ script.js
@@ -34,6 +34,34 @@ let page = {
     }
 }
 
+let yuque = {
+    getElement(){
+        this.box1 = document.getElementsByClassName("github")
+        this.box2 = document.getElementsByClassName("docs");
+        this.box3 = document.getElementsByClassName("yuque");
+        this.text = document.getElementsByClassName("yuquetext");
+    },
+    addEvent(){
+        this.getElement();
+        this.box3[0].addEventListener("mouseover",()=>{
+            this.box1[0].style.width = "12rem"
+            this.box2[0].className = "docs plus"
+            this.box3[0].style.height = "12rem"
+            this.box3[0].style.width = "18rem"
+            this.text[0].className = "yuquetext text"
+        },true)
+        this.box3[0].addEventListener("mouseout",()=>{
+            this.box1[0].style.width = "15rem"
+            this.box2[0].className = "docs"
+            this.box3[0].style.height = "6rem"
+            this.box3[0].style.width = "15rem"
+            this.text[0].className = "yuquetext"
+        },false)
+    }
+}
+
 window.onload=()=>{
     page.addEvent();
+    yuque.addEvent();
+    console.log(yuque)
 }
